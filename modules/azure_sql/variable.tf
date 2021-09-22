6dd3d8583925a55f1bfa09f1685a725799e1f2d6
Nikhilesh Chandika <Nikhilesh.Chandika@minterellison.com>
2021-09-23 08:03:44 +1000

    Create variable.tf

---
+++ modules/azure_sql/variable.tf
@@ -0,0 +1,23 @@
+# Variables For Resources
+
+variable "env" {}
+
+variable "environment" {}
+
+variable "location" {
+default     = "australiaeast"
+}
+
+#variable "hubsubscriptionid" {}
+
+#variable "hubsubscriptionname" {}
+
+#variable "diagnosticstoragename" {}
+
+variable "project_code_name" {}
+
+variable "sql_server_name" {}
+
+variable "sql_database_name" {}
+
+variable "storage_account_name" {}
